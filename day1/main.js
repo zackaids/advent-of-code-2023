e0c5fcdbf9aa29e9557c163f31c3301890e51e2a
zackaids <84846343+zackaids@users.noreply.github.com>
2023-12-05 01:18:36 -0500

    tried didnt word

--- day1/main.js
+++ day1/main.js
@@ -10,8 +10,18 @@ let test = [
     "1abc2", // 12
     "pqr3stu8vwx", // 38
     "a1b2c3d4e5f", // 15
-    "treb7uchet" // 77
+    "treb7uchet", // 77
+    
 ]
+let test2 = ["two1nine", // 29
+"eightwothree", // 83
+"abcone2threexyz", // 23
+"xtwone3four", // 24
+"4nineeightseven2", // 42
+"zoneight234", // 84
+"7pqrstsixteen", // 76
+];
+
 let numbers = {
     1: "1",
     2: "2",
@@ -22,13 +32,36 @@ let numbers = {
     7: "7",
     8: "8",
     9: "9",
-    0: "0"
+    0: "0",
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+    zero: '0'
 }
+
+const fs = require('fs')
+const readData = fs.readFileSync('./day1/input.txt', 'utf-8');
+const dataArray = readData.split('\n');
+
 const myMap = new Map(Object.entries(numbers));
+
 function getNumber(array) {
+    let str = array.join('');
+    for (let [key, value] of myMap) {
+        str = str.split(key).join(value);
+    }
+    // in case of "eightwothree" returns "eigh23"
+    console.log(str)
     let out = [];
-    for (let i=0; i<=array.length; i++) {
-        let temp = array[i].split('');
+    let sum = 0;
+    for (let i=0; i<str.length; i++) {
+        let temp = str[i].split("");
         let number = [];
         for (let j=0; j<=temp.length; j++) {
             if (myMap.has(temp[j])) {
@@ -36,10 +69,44 @@ function getNumber(array) {
             }
         }
         if (number.length > 2) {
-            
-        } else {
-
-        }
+            number = [number[0], number[number.length - 1]];
+        };
+        if (number.length === 1) {
+            number = [number[0], number[0]];
+        };
+        let joined = number.join('');
+        let updateNumber = parseInt(joined, 10);
+        out.push(updateNumber);
     }
+    for (let k=0; k<out.length; k++) {
+        sum += out[k];
+    }
+    return sum;
 }
-console.log(getNumber(test));
+
+console.log(getNumber(test2))
+
+
+
+// "one": '1',
+    // "two": '2',
+    // "three": '3',
+    // "four": '4',
+    // "five": '5',
+    // "six": '6',
+    // "seven": '7',
+    // "eight": '8',
+    // "nine": '9',
+    // "zero": '0',
+    // "ten": '10',
+    // "eleven": '11',
+    // "twelve": '12',
+    // "thirteen": '13',
+    // "fourteen": '14',
+    // "fifteen": '15',
+    // "sixteen": '16',
+    // "seventeen": '17',
+    // "eighteen": '18',
+    // "nineteen": '19',
+
+    
